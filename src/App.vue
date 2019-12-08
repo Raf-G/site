cc584a34c11535d93b5c967a074790ea79590632
Rovshan <raf-445@mail.ru>
2019-12-08 04:58:53 +0300

    Add menu

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,50 @@
 <template>
   <div id="app">
-    <div class="menu__wrap">
-      <button
-        onclick="myFunction()"
-        class="nav__burger"
-      />
-      <div id="nav">
-        <router-link
-          class="nav__item"
-          to="/"
-        >
-          Home
-        </router-link>
-        <router-link
-          class="nav__item"
-          to="/about"
-        >
-          About
-        </router-link>
-        <router-link
-          class="nav__item"
-          to="/test"
-        >
-          test
-        </router-link>
+    <header>
+      <a href="/" class="logo"><img src="@/assets/logo.jpg" alt="logo"></a>
+      <div
+        class="menu__wrap"
+        :class="{ nav__mobile: btnBurger }"
+      >
+        <button
+          class="nav__burger"
+          @click="myFunction"
+        />
+        <div id="nav">
+          <router-link
+            class="nav__item"
+            to="/"
+          >
+            Home
+          </router-link>
+          <router-link
+            class="nav__item"
+            to="/about"
+          >
+            About
+          </router-link>
+          <router-link
+            class="nav__item"
+            to="/test"
+          >
+            test
+          </router-link>
+        </div>
       </div>
-    </div>
+    </header>
     <router-view />
   </div>
 </template>
 <script>
 export default {
   data() {
+    return {
+      btnBurger: false,
+    };
   },
   methods: {
     myFunction() {
-      const element = document.getElementById('myDIV');
-      element.classList.toggle('mystyle');
+      this.btnBurger = !this.btnBurger;
     },
   },
 };
@@ -46,24 +54,45 @@ body, p, ul, li, h1,h2,h3,h4,ol,span,b,i {
     margin: 0;
     padding: 0;
   }
+  /* Start: Header */
+  header {
+    background-color: #002a32;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 30px;
+  }
+  .logo img {
+    width: 120px;
+    height: 120px;
+    border-radius: 50%;
+  }
   /* Start: Menu */
   .menu__wrap {
-    width: 100%;
-    margin: 40px auto;
+    display: inline-block;
     text-align: center;
   }
   .nav__item {
-    background-color: rgb(106, 49, 240);
     font-size: 35px;
     color: #fff;
     text-decoration: none;
     padding: 10px 15px;
     margin: 0 10px;
+    border-radius: 5px;
+    transition: 0.3s;
+  }
+  .nav__item:hover {
+    box-shadow: #002a32;
+    -webkit-box-shadow: 0px 0px 40px 0px rgba(0,0,0,0.75);
+    -moz-box-shadow: 0px 0px 40px 0px rgba(0,0,0,0.75);
+    box-shadow: 0px 0px 40px 0px rgba(0,0,0,0.75);
+    transition: 0.3s;
   }
   .nav__burger {
     display: none;
   }
   /* End: Menu */
+  /* End: Header */
   @media (max-width: 768px) {
     .nav__burger {
       display: block;
@@ -77,12 +106,11 @@ body, p, ul, li, h1,h2,h3,h4,ol,span,b,i {
         margin-bottom: 10px;
         display: none;
     }
-    .mystyle {
-      width: 100%;
-      padding: 25px;
-      background-color: coral;
-      color: white;
-      font-size: 25px;
+    .nav__mobile .nav__burger {
+      background-color: red;
+    }
+    .nav__mobile .nav__item {
+      display: inline-block;
     }
   }
 </style>
